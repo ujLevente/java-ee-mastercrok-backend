63fb8314067c37e83281133ddedefa52d5c8f74f
Uj Levente <asdlevente@gmail.com>
2019-02-20 14:38:30 +0100

    gameplay module config(resttemplate bean), models, service added

--- service-socket-handler/src/main/java/com/codecool/websocket/controller/MessageController.java
+++ service-socket-handler/src/main/java/com/codecool/websocket/controller/MessageController.java
@@ -7,7 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
 
