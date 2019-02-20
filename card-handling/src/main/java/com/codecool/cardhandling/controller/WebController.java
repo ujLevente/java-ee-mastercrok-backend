d1bbb0ea13432f887d8e356a024cedc5d1fa2401
adamszabolcs <szabolcs.adam91@gmail.com>
2019-02-20 10:40:21 +0100

    created route to show images

--- card-handling/src/main/java/com/codecool/cardhandling/controller/WebController.java
+++ card-handling/src/main/java/com/codecool/cardhandling/controller/WebController.java
@@ -1,4 +1,48 @@
 package com.codecool.cardhandling.controller;
 
+import com.codecool.cardhandling.service.FileStorageService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@RestController
+@Slf4j
 public class WebController {
+
+    @Autowired
+    FileStorageService fileStorageService;
+
+    @GetMapping(value = "/images/{fileName:.+}", produces = MediaType.ALL_VALUE)
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
+        // Load file as Resource
+        Resource resource = fileStorageService.loadFileAsResource(fileName);
+        log.debug("Requested " + resource + " file");
+        // Try to determine file's content type
+        String contentType = null;
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (IOException ex) {
+            log.info("Could not determine file type.");
+        }
+
+        // Fallback to the default content type if type could not be determined
+        if (contentType == null) {
+            contentType = "application/octet-stream";
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.IMAGE_PNG)
+                .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
+    }
+
 }
