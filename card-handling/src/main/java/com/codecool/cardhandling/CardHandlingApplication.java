147e8c9bb05bd02ab4d2a70b0723631061580d2f
adamszabolcs <szabolcs.adam91@gmail.com>
2019-02-20 10:37:04 +0100

    added EnableConfigurationProperties annotation

--- card-handling/src/main/java/com/codecool/cardhandling/CardHandlingApplication.java
+++ card-handling/src/main/java/com/codecool/cardhandling/CardHandlingApplication.java
@@ -1,9 +1,14 @@
 package com.codecool.cardhandling;
 
+import com.codecool.cardhandling.property.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+        FileStorageProperties.class
+})
 public class CardHandlingApplication {
 
     public static void main(String[] args) {
