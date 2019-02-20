2e5f0612af77788f2147e2b548f70960b535f5e1
adamszabolcs <szabolcs.adam91@gmail.com>
2019-02-20 17:00:50 +0100

    extended with variables

--- game-status/src/main/java/com/codecool/gamestatus/model/Game.java
+++ game-status/src/main/java/com/codecool/gamestatus/model/Game.java
@@ -7,8 +7,13 @@ import java.util.Queue;
 @Data
 public class Game {
 
+    private String id;
+
     private Queue<CardServiceResult> playerOneCardList;
     private Queue<CardServiceResult> playerTwoCardList;
+
+    private Player playerOne;
+    private Player playerTwo;
 
 
 
