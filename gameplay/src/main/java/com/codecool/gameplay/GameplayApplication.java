19fb65ecd46ca82d0e3b7328bf0e5a07242fcbd9
Uj Levente <asdlevente@gmail.com>
2019-02-20 13:48:10 +0100

    gameplay module added

---
+++ gameplay/src/main/java/com/codecool/gameplay/GameplayApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.gameplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameplayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameplayApplication.class, args);
+    }
+
+}
