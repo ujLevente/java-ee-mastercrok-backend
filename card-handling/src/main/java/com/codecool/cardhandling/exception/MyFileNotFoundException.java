ca38080520a191d7667cffae425b324eebb83193
adamszabolcs <szabolcs.adam91@gmail.com>
2019-02-20 12:14:55 +0100

    created exception class

--- card-handling/src/main/java/com/codecool/cardhandling/exception/MyFileNotFoundException.java
+++ card-handling/src/main/java/com/codecool/cardhandling/exception/MyFileNotFoundException.java
@@ -1,4 +1,15 @@
 package com.codecool.cardhandling.exception;
 
-public class MyFileNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class MyFileNotFoundException extends RuntimeException {
+    public MyFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public MyFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
