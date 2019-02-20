01455164deef043cba15b5584c776e306c3fb761
adamszabolcs <szabolcs.adam91@gmail.com>
2019-02-20 09:24:11 +0100

    card-handling microservice created

---
+++ card-handling/src/main/java/com/codecool/cardhandling/CardHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.cardhandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardHandlingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardHandlingApplication.class, args);
+    }
+
+}
