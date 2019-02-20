30aa56a7a29e84d937c06aaeb6dabb77a2a544d3
Uj Levente <asdlevente@gmail.com>
2019-02-20 11:00:45 +0100

    create game rest api implemented

--- service-socket-handler/src/main/java/lahsivjar/spring/websocket/template/MessageController.java
+++ service-socket-handler/src/main/java/lahsivjar/spring/websocket/template/MessageController.java
@@ -1,27 +1,27 @@
 package lahsivjar.spring.websocket.template;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
+@Slf4j
 public class MessageController {
 
     @Autowired
     private ChatHistoryDao chatHistoryDao;
 
-
     @Autowired
     private SimpMessagingTemplate template;
+
+    private Set<String> gameIds = new HashSet<>();
 
     /*
      * This MessageMapping annotated method will be handled by
@@ -36,9 +36,18 @@ public class MessageController {
         template.convertAndSend("/topic/"+message.get("authorId"), message);
     }
 
-    @RequestMapping("/history")
-    public List<Map<String, String>> getChatHistory() {
-        System.out.println("Bármi");
-        return chatHistoryDao.get();
+
+    @RequestMapping("/create-game/{gameId}")
+    public HttpStatus create(@PathVariable String gameId) {
+        log.info("starting game with gameId: " + gameId);
+        gameIds.add(gameId);
+        return HttpStatus.OK;
     }
+
+//    @RequestMapping("/start/{gameId}")
+//    public List<Map<String, String>> getChatHistory() {
+//        System.out.println("Bármi");
+//        return chatHistoryDao.get();
+//        template.convertAndSend("/topic/${gameId}", {start });
+//    }
 }
