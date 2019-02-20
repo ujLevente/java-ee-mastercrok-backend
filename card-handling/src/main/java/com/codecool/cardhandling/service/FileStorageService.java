8cfd3c120032acc34ec34e7d32742a2efe3a27bd
adamszabolcs <szabolcs.adam91@gmail.com>
2019-02-20 10:42:58 +0100

    created file for loading file as resource

--- card-handling/src/main/java/com/codecool/cardhandling/service/FileStorageService.java
+++ card-handling/src/main/java/com/codecool/cardhandling/service/FileStorageService.java
@@ -1,4 +1,50 @@
 package com.codecool.cardhandling.service;
 
+import com.codecool.cardhandling.property.FileStorageProperties;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+
+import java.io.FileNotFoundException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Service
+@Slf4j
 public class FileStorageService {
+
+    private final Path fileStorageLocation;
+
+    @Autowired
+    public FileStorageService(FileStorageProperties fileStorageProperties) {
+        this.fileStorageLocation = Paths.get(fileStorageProperties.getImagesDir())
+                .toAbsolutePath().normalize();
+
+        try {
+            Files.createDirectories(this.fileStorageLocation);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public Resource loadFileAsResource(String filename) {
+        Resource resource = null;
+        try {
+            Path filePath = this.fileStorageLocation.resolve(filename).normalize();
+            resource = new UrlResource(filePath.toUri());
+            if (resource.exists()) {
+                return resource;
+            } else {
+                throw new FileNotFoundException("File not found");
+            }
+        } catch (MalformedURLException | FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return resource;
+    }
+
 }
