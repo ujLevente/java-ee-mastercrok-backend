a0c836def850d83b85605d8e147d76b5dc34a2d0
adamszabolcs <szabolcs.adam91@gmail.com>
2019-02-20 15:02:47 +0100

    created game-status module

---
+++ game-status/src/main/java/com/codecool/gamestatus/GameStatusApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.gamestatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameStatusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameStatusApplication.class, args);
+    }
+
+}
